ff1cfad2da2a9b4eefee1647d92b156be108bc62
PAOLO BO <PAOLOBO@gefit-fubine.com>
2026-03-29 00:25:22 -0700

    Aggiornamento veloce

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,12 +1,7 @@
-import { useAdmin } from '@/hooks/useAdmin'
-
 export default function EditorPage() {
-  const { checkAdmin } = useAdmin()
-
-  // Se non admin, redirect o mostra messaggio
-  if (!checkAdmin()) {
-    return <div>Accesso negato. Solo admin.</div>
-  }
-
-  // ... resto del codice GrapesJS
+  return (
+    <div>
+      {/* ... resto del codice GrapesJS */}
+    </div>
+  )
 }
